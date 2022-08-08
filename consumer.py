8d69c733406dfaa2cd4a5f1a516ccb6ee375f1ea
harikan23 <36985848+harikan23@users.noreply.github.com>
2022-08-07 20:32:35 -0500

    kafka consumer

---
+++ consumer.py
@@ -0,0 +1,36 @@
+from kafka import KafkaConsumer
+from json import loads
+
+import models
+
+
+def consumerData():
+    consumer = KafkaConsumer(
+        'topicuser',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        auto_commit_interval_ms=1000,
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+    for message in consumer:
+        userData = message.value
+
+        print("user data")
+        print(userData)
+
+        models.User.create_user(
+            username=userData["username"],
+            email=userData["email"],
+            password=userData["password"],
+            current_role=userData["curent_role"],
+            current_company=userData["current_company"],
+            first_name=userData["first_name"],
+            last_name=userData["last_name"],
+            skills=userData["skills"],
+            description=userData["description"]
+        )
+        print('registration successful-', userData)
+
+
+if __name__ == '__main__':
+    consumerData()
